a6b63eef5b0e667dd380814109b75126c1debc71
Etah <arrahetah23@gmail.com>
2023-01-31 23:56:17 +0100

    detail page

---
+++ src/components/DetailSnippet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetailSnippet = () => {
+  return (
+    <div>DetailSnippet</div>
+  )
+}
+
+export default DetailSnippet
--- src/pages/DetailedPage.js
+++ src/pages/DetailedPage.js
@@ -3,11 +3,24 @@ import { useParams } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import videoData from '../data/videoData'
 import '../assets/styles/detailPage.css';
-import {MdGppGood} from 'react-icons/md'
+import {MdGppGood, MdOutlinePlaylistAdd} from 'react-icons/md';
+import {BsHandThumbsUp, 
+  BsFillHandThumbsUpFill,
+  BsHandThumbsDown,
+  BsHandThumbsDownFill
+} from 'react-icons/bs';
+import {BiShare} from 'react-icons/bi';
+import {AiOutlineScissor} from 'react-icons/ai';
 import ReactPlayer from 'react-player'
+import Categories from '../components/Categories';
+import DetailSnippet from '../components/DetailSnippet';
 const DetailedPage = ({visible}) => {
     const[sidebar,setSidebar]= useState(true);
-    const[video,setVideo]=useState({})
+    const[video,setVideo]=useState({});
+    const [like,setLike]= useState({
+      like:false,
+      disLike:false
+    })
     // const[]=useState(false)
     const {id}= useParams();
     console.log(id)
@@ -16,7 +29,14 @@ const DetailedPage = ({visible}) => {
         item.id === id
       ))
     setVideo(data[0])
-    },[])
+    },[]);
+    const likeClick=()=>{
+      setLike({...like,like:!like.like,disLike:false})
+    }
+    const dislikeClick =()=>{
+      setLike({...like,like:false,disLike:!like.disLike})
+    }
+
   return (
     <div className='detailContainer'>
             {visible &&  
@@ -44,29 +64,50 @@ const DetailedPage = ({visible}) => {
         />
         <h3>{video?.title}</h3>
         <div className='detailTagBar'>
-          <div>
+          <div className='tagLeftSide'>
           <img src={video?.thumbnail} alt='owner profile'/>
           <div>
             <span>
             <p>{video?.owner}</p>
             {video?.verified && <MdGppGood size={30} color="white"/>}
             </span>
-            <p>{video?.subscribers[0]+ video.subscribers[1]}</p>
+            <p>{video?.subscribers} subscribers</p>
           </div>
-          <button>Subscribe</button>
+          <button className='subscribe'>Subscribe</button>
           </div>
-          <div>
-            <button>{video?.likes[0]+ video.likes[1]}</button>
-            <button>Share</button>
-            <button>Download</button>
-            <button>Clip</button>
-            <button>Save</button>
-            <button> &#8226; &#8226; &#8226;</button>
+          <div className='tagRightSide'>
+            <div className='likeContainer'>
+              <button onClick={likeClick}> {like.like?
+              <BsFillHandThumbsUpFill size={20} color="white"/>:
+              <BsHandThumbsUp size={20} color="white" />
+              }</button>
+              <p>{video?.likes}</p>
+              <button onClick={dislikeClick}>
+              {like.disLike?
+              <BsHandThumbsDownFill size={20} color="white"/>:
+              <BsHandThumbsDown size={20} color="white" />
+              }
+              </button>
+              </div>
+            <button className='share'>
+              <p>Share</p>
+              <BiShare size={30} color='white'/>
+            </button>
+            {/* <button>Download</button> */}
+            <button className='share'>
+              <AiOutlineScissor size={20} color="white"/>
+              <p> Clip</p>
+              </button>
+            <button className='share'>
+              <MdOutlinePlaylistAdd size={20} color='white'/>
+              <p>Save</p>
+            </button>
+            <button className='share'> &#8226; &#8226; &#8226;</button>
           </div>
         </div>
         <div className='detailDescription'>
           <span>
-          <p>{video?.views[0]+ video.views[1]}</p>
+          <p>{video?.views}</p>
           <p>{video?.duration}</p>
           </span>
           <p>{video?.description}</p>
@@ -78,7 +119,7 @@ const DetailedPage = ({visible}) => {
    <div className='detailLeftBottom'>
    <div className='tags'>
           <p>Tags</p>
-          {video?.category.map((tag,index)=>(
+          {video?.category?.map((tag,index)=>(
             <p key={index}>
               {tag}
             </p>
@@ -87,7 +128,14 @@ const DetailedPage = ({visible}) => {
    </div>
        </div>
        <div className='detailRight'>
-
+<Categories/>
+<div className='detailVideos'>
+{
+  videoData.map((item,index)=>(
+    <DetailSnippet/>
+  ))
+}
+</div>
        </div>
         </div>
         </div>
