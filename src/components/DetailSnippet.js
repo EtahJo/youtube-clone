1c6c1c3ec9f39f9d5ff4524606c881c4eef86188
Etah <arrahetah23@gmail.com>
2023-02-02 22:42:22 +0100

    detail page

--- src/components/DetailSnippet.js
+++ src/components/DetailSnippet.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import {BsStopwatch,} from 'react-icons/bs';
+import {RiPlayList2Fill} from 'react-icons/ri';
 
 const DetailSnippet = ({img,title,owner , views,duration,id}) => {
     const navigate = useNavigate();
   return (
     <div className='detailSnippet' onClick={()=>navigate(`/detail/${id}`)}>
-        <div>
+        <div className='detailSnippetLeft'>
             <img src={img} alt="Thumbnail"/>
+            <span>
+                <BsStopwatch size={20} color="white" className='spanIcon'/>
+                <RiPlayList2Fill size={20} color="white"  className='spanIcon'/>
+            </span>
         </div>
         <div className='detailSnippetContent'>
             <h3>{title}</h3>
