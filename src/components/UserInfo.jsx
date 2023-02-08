29a9d0788519698a135e969235df8a8024e146e8
Etah <arrahetah23@gmail.com>
2023-02-08 23:35:18 +0100

    user info modal

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const UserInfo = () => {
   return (
-    <div className='userInfo'>UserInfo</div>
+    <div className='userInfo'>
+        <div className='userHeader'>
+            <img  src='https://lh3.googleusercontent.com/ogw/AOh-ky3XFUqj4N-jxZTxj_HXmL6_rpAzPTtQJnGOUUkUxQ=s64-c-mo' alt='user profile'/>
+            <div>
+                <p>Arrah Etah</p>
+                <p>@arrah-etah</p>
+                <a href='/#'>Manage your google account</a>
+            </div>
+        </div>
+    </div>
   )
 }
 
